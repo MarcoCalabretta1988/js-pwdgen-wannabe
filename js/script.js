1ef12f6427036d63311e34b5028aa4822de04ab5
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2022-12-02 15:12:32 +0100

    Add initial comments

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK');
+
+//Chiedi all'utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. In fine scivi sulla pagina nomecognomecolore preferito21
+
+
+/* 
+
+TO DO LIST:
+1- Prendo l'elemento dalla pagina
+2- Creo e assegno il valore alla costante dell aggiunta finale del 21
+3- Chiedere nome all'utente e salvarlo in una variabile
+4- Chiedere cognome all'utente e salvarlo in una variabile
+5- Chiedere colore preferito all'utente e salvarlo in una variabile
+6- Creare una variabile in cui addiziono tutte le stringhe
+7- Stampo l'elemento recuperato con il valore calcolato precedentemente
+
+*/
+
